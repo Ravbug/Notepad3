49134bc6ddae573ac1675557fa38ef1b5286690c
Ravbug <ravbuganimations@gmail.com>
2019-03-30 19:04:56 -0700

    + Added RTF/TXT mode switching + Added menu item to switch modes

--- Notepad3/MainWindow.xaml.cs
+++ Notepad3/MainWindow.xaml.cs
@@ -20,9 +20,94 @@ namespace Notepad3
     /// </summary>
     public partial class MainWindow : Window
     {
+        //enum for text editing mode
+        enum TextMode{RTF,TXT}
+        static TextMode mode = TextMode.TXT;
         public MainWindow()
         {
             InitializeComponent();
+            //by default loads the RTF view
+            setTextMode(TextMode.RTF);
+        }
+
+        /// <summary>
+        /// Returns the current active text editing view (RTF or TXT)
+        /// </summary>
+        /// <returns>System.Windows.Controls.Control current view</returns>
+        private System.Windows.Controls.Control getCurrentEditor()
+        {
+            return getControlForEnum(mode);
+        }
+        
+        /// <summary>
+        /// Returns the editing control for a passed Enum
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        private System.Windows.Controls.Control getControlForEnum(TextMode m)
+        {
+            if (m == TextMode.RTF)
+            {
+                return rtfView;
+            }
+            else
+            {
+                return txtView;
+            }
+        }
+
+        /// <summary>
+        /// Toggles between RTF editing and TXT editing
+        /// </summary>
+        private void toggleTextMode()
+        {
+            TextMode mTemp = (mode == TextMode.RTF) ? TextMode.TXT : TextMode.RTF;
+            setTextMode(mTemp);
+        }
+
+        /// <summary>
+        /// Switches the current editing mode between TXT and RTF
+        /// </summary>
+        /// <param name="newMode">Enum representing the new mode to switch to</param>
+        private void setTextMode(TextMode newMode)
+        {
+            if (newMode == mode) { return; }
+            //prep the controls for switch
+            Control old = getCurrentEditor();
+            Control current;
+            if (newMode == TextMode.RTF)
+            {
+                //load the string into the RTF view
+                rtfView.Document.Blocks.Clear();
+                rtfView.Document.Blocks.Add(new Paragraph(new Run(txtView.Text)));
+                current = rtfView;
+            }
+            else
+            {
+                //warn the user with a popup dialog
+
+                //load the text
+                string richText = new TextRange(rtfView.Document.ContentStart, rtfView.Document.ContentEnd).Text;
+                txtView.Text = richText;
+                current = txtView;
+            }
+            //switch the views
+            current.IsEnabled = true;
+            current.Visibility = System.Windows.Visibility.Visible;
+
+            old.IsEnabled = false;
+            old.Visibility = System.Windows.Visibility.Hidden;
+            mode = newMode;
+        }
+
+        //================================UI click handlers==============================
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == toggleEdit)
+            {
+                toggleTextMode();
+            }
         }
     }
 }
